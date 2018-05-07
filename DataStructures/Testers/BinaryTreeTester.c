e94c140d16be33dfc2aca9cb08f4c628c2b2c50c
jairo13c <31705891+jairo13c@users.noreply.github.com>
2018-05-07 11:40:40 -0600

    binary tree

---
+++ DataStructures/Testers/BinaryTreeTester.c
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeTester.c
+//  Data Structures
+//
+//  Created by Cortez, Jairo on 4/11/18.
+//  Copyright © 2018 Cortez, Jairo. All rights reserved.
+//
+
+#include "BinaryTreeTester.h"
---
+++ DataStructures/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,21 @@
+//
+//  BinaryTreeTester.cpp
+//  Data Structures
+//
+//  Created by Cortez, Jairo on 3/27/18.
+//  Copyright © 2018 Cortez, Jairo. All rights reserved.
+//
+
+#include "../model/Nodes/BinaryTreeNode.hpp"
+#include <iostream>
+
+class BinaryTreeTester
+{
+private:
+    BinaryTreeNode<int> testNode;
+    
+    
+    
+};
+
+
